2f291ed5af134fd57d4f07695067d0e0b8e27132
Joe Hawes <morehawes@gmail.com>
2022-06-27 10:33:23 -0700

    Joe Config start

--- Joe.php
+++ Joe.php
@@ -4,6 +4,10 @@ add_filter('the_content', function($c) {
 	return '<p style="color:red">Joe!</p>' . $c;
 });
 
-require_once('Helpers/Joe_Helper.php');
-require_once('Helpers/Joe_Input.php');
-require_once('Helpers/Joe_Cache.php');
+//Helpers
+require_once('Helpers/Helper.php');
+require_once('Helpers/Input.php');
+require_once('Helpers/Cache.php');
+
+//Core
+require_once('Core/Config.php');
