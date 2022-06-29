9e5a0d63f5ded55392553d815d91d7756e4a40d6
Joe Hawes <morehawes@gmail.com>
2022-06-29 14:48:20 -0700

    AJAX 4

--- Core/AJAX.php
+++ Core/AJAX.php
@@ -1,10 +1,5 @@
 <?php
 
 class Joe_AJAX {
-	function __construct() {
-		//Add nonce
-		add_action('init', function() {
-			Joe_JS::add_chunk('var ' . Joe_Config::get_item('plugin_slug') . '_security = "' . wp_create_nonce(Joe_Config::get_item('nonce_string')) . '";');					
-		});
-	}
+	function __construct() {}
 }
