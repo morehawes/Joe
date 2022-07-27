e33003e4cb7053fa9025096a55f4708f442d5bbb
Joe Hawes <morehawes@gmail.com>
2022-07-27 09:20:11 -0700

    1

--- inc/Cache.php
+++ inc/Cache.php
@@ -9,43 +9,49 @@ class Joe_Cache {
 	}
 	
 	static function get_item($cache_id) {
-		self::backup_item($cache_id);
-
 		return get_transient(Joe_Helper::slug_prefix($cache_id, '_', false));
 	}
 
-	static function backup_item($cache_id) {
+	static function get_stale($cache_id) {
 		global $wpdb;
 
-		$timeout = $wpdb->get_var(
+		$stale = [];
+
+		$results = $wpdb->get_results(
 			$wpdb->prepare("
-				SELECT option_value
+				SELECT option_name, option_value
 				FROM $wpdb->options
-				WHERE option_name = '%s'
-			", '_transient_timeout_' . Joe_Helper::slug_prefix($cache_id, '_', false)
+				WHERE option_name LIKE '%s'
+			", '_transient_%' . Joe_Helper::slug_prefix($cache_id, '_', false)
 			)
-		);
+		, ARRAY_A);
 		
-// 		$query = "
-// 			SELECT *
-// 			FROM " . $wpdb->options . "
-// 			WHERE option_name LIKE '_transient_%" . Joe_Helper::slug_prefix($cache_id, '_', false) . "%'";
-// 		
-// 		$result = $wpdb->get_results($query);
-
-// 		$query = sprintf("
-// 			SELECT option_value
-// 			FROM $wpdb->options
-// 			WHERE option_name = '_transient_timeout_%s'
-//     ", Joe_Helper::slug_prefix($cache_id, '_', false)
-// 		);
-
- 		Joe_Helper::debug($timeout, false);
+		if(sizeof($results)) {
+			foreach($results as $result) {
+				if($result['option_name'] == '_transient_timeout_' . Joe_Helper::slug_prefix($cache_id, '_', false)) {
+					$stale['timeout'] = $result['option_value']; 			
+				} elseif($result['option_name'] == '_transient_' . Joe_Helper::slug_prefix($cache_id, '_', false)) {
+					$stale['value'] = $result['option_value']; 			
+				}
+			}
+		
+			//Both are required
+			if(isset($stale['timeout']) && isset($stale['value'])) {
+				$stale['stale_seconds'] = time() - $stale['timeout'];
+			
+				//Has expired
+				if($stale['stale_seconds']) {
+					return $stale;
+				}
+			}
+		} 		
+		
+ 		return $stale;
 	}
 	
-	static function flush() {
-		global $wpdb;
-		
-		//$wpdb->query("DELETE FROM " . $wpdb->options . " WHERE option_name LIKE '_transient_%" . self::$cache_prefix . "%'");
-	}
+// 	static function flush() {
+// 		global $wpdb;
+// 		
+// 		$wpdb->query("DELETE FROM " . $wpdb->options . " WHERE option_name LIKE '_transient_%" . self::$cache_prefix . "%'");
+// 	}
 }
