0477fe4a49519e17e578effcee896fd7eec8ed1a
Joe Hawes <morehawes@gmail.com>
2022-07-27 11:06:26 -0700

    2

--- inc/Cache.php
+++ inc/Cache.php
@@ -8,45 +8,58 @@ class Joe_Cache {
 		set_transient(Joe_Helper::slug_prefix($cache_id, '_', false), $cache_content, $cache_seconds);						
 	}
 	
-	static function get_item($cache_id) {
-		return get_transient(Joe_Helper::slug_prefix($cache_id, '_', false));
-	}
+	static function get_item($cache_id, $check_stale = false) {
+		//Straight WP caching baby...
+		if(! $check_stale) {
+			return get_transient(Joe_Helper::slug_prefix($cache_id, '_', false));
+		//Joe implementation
+		} else {	
+			global $wpdb;
 
-	static function get_stale($cache_id) {
-		global $wpdb;
-
-		$stale = [];
-
-		$results = $wpdb->get_results(
-			$wpdb->prepare("
-				SELECT option_name, option_value
-				FROM $wpdb->options
-				WHERE option_name LIKE '%s'
-			", '_transient_%' . Joe_Helper::slug_prefix($cache_id, '_', false)
-			)
-		, ARRAY_A);
-		
-		if(sizeof($results)) {
-			foreach($results as $result) {
-				if($result['option_name'] == '_transient_timeout_' . Joe_Helper::slug_prefix($cache_id, '_', false)) {
-					$stale['timeout'] = $result['option_value']; 			
-				} elseif($result['option_name'] == '_transient_' . Joe_Helper::slug_prefix($cache_id, '_', false)) {
-					$stale['value'] = $result['option_value']; 			
-				}
-			}
-		
-			//Both are required
-			if(isset($stale['timeout']) && isset($stale['value'])) {
-				$stale['stale_seconds'] = time() - $stale['timeout'];
+			$results = $wpdb->get_results(
+				$wpdb->prepare("
+					SELECT option_name, option_value
+					FROM $wpdb->options
+					WHERE option_name LIKE '%s'
+				", '_transient_%' . Joe_Helper::slug_prefix($cache_id, '_', false)
+				)
+			, ARRAY_A);
 			
-				//Has expired
-				if($stale['stale_seconds']) {
-					return $stale;
+			//Valid response
+			if(sizeof($results) == 2) {
+				//Get our values
+				foreach($results as $result) {
+					if($result['option_name'] == '_transient_timeout_' . Joe_Helper::slug_prefix($cache_id, '_', false)) {
+						$timeout = $result['option_value']; 			
+					} elseif($result['option_name'] == '_transient_' . Joe_Helper::slug_prefix($cache_id, '_', false)) {
+						$value = $result['option_value']; 			
+					}
 				}
-			}
-		} 		
 		
- 		return $stale;
+				//Both are required
+				if(isset($timeout) && isset($value)) {
+					$time_to_expire = $timeout - time();
+					
+					//Fresh
+					if($time_to_expire > 0) {
+						return [
+							'status' => 'fresh',
+							'minutes' => ($time_to_expire / 60),
+							'value' => $value
+						];
+					//Stale
+					} else {
+						return [
+							'status' => 'stale',
+							'minutes' => ((0 - $time_to_expire) / 60),	//Negate
+							'value' => $value
+						];
+					}
+				}
+			} 	
+		}	
+		
+ 		return false;
 	}
 	
 // 	static function flush() {
